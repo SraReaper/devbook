5fad176ef4b0fa87a99a0e70f96841876bd8f3f9
SraReaper <carolin.cosgirl@gmail.com>
2024-03-14 16:03:29 -0400

    feat: rendering functions were created

--- main.go
+++ main.go
@@ -5,12 +5,13 @@ import (
 	"log"
 	"net/http"
 	"webapp/src/router"
+	"webapp/src/utils"
 )
 
 func main() {
-	fmt.Printf("WebApp inicializado...")
-
+	utils.CarregarTemplates()
 	r := router.Gerar()
 
+	fmt.Printf("WebApp inicializado na porta 3003...")
 	log.Fatal(http.ListenAndServe(":3003", r))
 }
