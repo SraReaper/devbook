87d4edac0ed5acc9185ec98cd2bd442ce9c964be
SraReaper <carolin.cosgirl@gmail.com>
2024-03-21 12:51:43 -0400

    feat: functions that execute template were created

---
+++ src/controllers/paginas.go
@@ -0,0 +1,16 @@
+package controllers
+
+import (
+	"net/http"
+	"webapp/src/utils"
+)
+
+// CarregarTelaDeLogin vai renderizar a tela de login
+func CarregarTelaDeLogin(w http.ResponseWriter, r *http.Request) {
+	utils.ExecutarTemplate(w, "login.html", nil)
+}
+
+// CarregarPaginaDeCadastroDeUsuario vai carregar a página de cadastro do usuario
+func CarregarPaginaDeCadastroDeUsuario(w http.ResponseWriter, r *http.Request) {
+	utils.ExecutarTemplate(w, "cadastro.html", nil)
+}
